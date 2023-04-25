d43727aeb3769638392ba23e2c226f9c273438f5
Yaroslava <yuriandreev1ch@gmail.com>
2023-04-25 14:33:03 +0300

    first commit

---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+import {useNavigate} from 'react-router-dom '
+
+function CreatePost() {
+  return (
+    <div>CreatePost</div>
+  )
+}
+
+export default CreatePost
